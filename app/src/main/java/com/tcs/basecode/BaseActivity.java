c700572684d9e844a5b3d811b6f4b6a026e18cbe
unknown <587823@01hw477497.TCSMOBILITY.COM>
2016-04-12 19:14:51 +0530

    Initial commit

---
+++ app/src/main/java/com/tcs/basecode/BaseActivity.java
@@ -0,0 +1,17 @@
+package com.tcs.basecode;
+
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by 587823 on 4/12/2016.
+ */
+public class BaseActivity extends AppCompatActivity{
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ButterKnife.unbind(this);
+    }
+}
---
+++ app/src/main/java/com/tcs/basecode/fragment/FirstFragment.java
@@ -0,0 +1,25 @@
+package com.tcs.basecode.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tcs.basecode.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FirstFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first_layout, container, false);
+    }
+
+}
