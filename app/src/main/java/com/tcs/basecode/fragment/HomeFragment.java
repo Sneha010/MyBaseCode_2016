fcbd3835664f2dcbfe7136152891c2c3e00cc49c
Sneha010 <sneha.khadatare@gmail.com>
2016-04-27 13:11:40 +0530

    Added home fragment

---
+++ app/src/main/java/com/tcs/basecode/fragment/HomeFragment.java
@@ -0,0 +1,70 @@
+package com.tcs.basecode.fragment;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tcs.basecode.R;
+import com.tcs.basecode.activity.MainActivity;
+import com.tcs.basecode.utilities.GeneralUtils;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends BaseFragment {
+
+    public static HomeFragment newInstance() {
+        HomeFragment fragment = new HomeFragment();
+        return fragment;
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        setUpMyToolbar();
+
+    }
+
+    private void setUpMyToolbar() {
+
+        GeneralUtils.changeHamburgderIcon(getResources().getColor(R.color.colorAccent) ,
+                ((MainActivity)getActivity()).getToolbar());
+        GeneralUtils.setToolbarStyle(getActivity() , ((MainActivity) getActivity()).getMyActionBar() ,
+                R.color.transparent , getTitle());
+    }
+
+
+    @Override
+    public String getTitle() {
+        return "";
+    }
+
+}
